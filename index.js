fd171597e0ee1248fe4253b7870d9b87f0be5dad
educad-uk <147948581+educad-uk@users.noreply.github.com>
2023-11-01 14:22:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ var lorem = new LoremIpsum({
   }
 });
 
+function redirectWwwTraffic(req, res, next) {
+  if (req.headers.host.slice(0, 4) === "www.") {
+    var newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + "://" + newHost + req.originalUrl);
+  }
+  next();
+}
+
+app.set("trust proxy", true);
+app.use(redirectWwwTraffic);
+
 app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

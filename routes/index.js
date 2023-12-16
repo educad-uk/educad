084459131537ace7ec1052ec6c83b2050e63faf0
educad-uk <147948581+educad-uk@users.noreply.github.com>
2023-12-16 02:01:27 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,196 +1,97 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const Mail = require("nodemailer");
+const bodyParser = require("body-parser");
+const Handlebars = require("handlebars");
 
-var MersenneTwister = require('mersenne-twister');
-var generator = new MersenneTwister();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+var readHTMLFile = function (path, callback) {
+  fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, html);
+    }
+  });
+};
+
+var MailObject = Mail.createTransport({
+  service: "gmail",
+  auth: {
+    user: "educadtuition@gmail.com",
+    pass: "ocqg uppp fish prip",
+  },
+});
+
+router..post("/send", (Request, Response) => {
+  readHTMLFile("./public/contactDetailsEmail.html", function (err, html) {
+    if (err) {
+      console.log("error reading file", err);
+      return;
+    }
+    var template = Handlebars.compile(html);
+    var replacements = {
+      firstName: Request.body.firstName,
+      lastName: Request.body.lastName,
+      contactNumber: Request.body.contactNumber,
+      tuitionType: Request.body.tuitionType,
+      email: Request.body.email,
+    };
+    var htmlToSend = template(replacements);
+
+    var date = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+    var MailQuery = {
+      to: "educadtuition@gmail.com",
+      subject:
+        "New Application (#" +
+        Math.floor(Math.random() * 10000000000 + 1) +
+        ")",
+      html: htmlToSend,
+    };
+
+    MailObject.sendMail(MailQuery, function (error, info) {
+      if (error) {
+        console.log(error);
+        Response.sendFile(__dirname + "/public/404.html");
+      } else {
+        console.log("Email sent: " + info.response);
+        Response.sendFile(__dirname + "/public/index.html");
+      }
+    });
+
+    var MailClient = {
+      to: Request.body.email,
+      subject: "We have receieved your application",
+      text:
+        Request.body.firtName +
+        ", " +
+        Request.body.lastName +
+        ", " +
+        Request.body.contactNumber +
+        ", " +
+        Request.body.tuitionType,
+    };
+
+    MailObject.sendMail(MailClient, function (error, info) {
+      if (error) {
+        console.log(error);
+        Response.sendFile(__dirname + "/public/404.html");
+      } else {
+        console.log("Email sent: " + info.response);
+        Response.sendFile(__dirname + "/public/index.html");
+      }
+    });
+  });
+});
 
 
 router.get('/', function(req, res, next) {
    res.render('index');
 });
-
-
-// import { LoremIpsum } from 'lorem-ipsum';
-const LoremIpsum = require('lorem-ipsum').LoremIpsum;
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-function randomArticle() {
-  var articles = ['a', 'the'];
-  return articles[Math.floor(generator.random() * articles.length)];
-}
-
-function randomNoun() {
-  var nouns = ['waste','increase','sky','horn','sweater','head','push','bell'];
-  return nouns[Math.floor(generator.random() * nouns.length)];
-}
-
-function randomVerb() {
-  var verbs = ['prays','sprouts','opens','plants','shades','signals','shivers'];
-  return verbs[Math.floor(generator.random() * verbs.length)];
-}
-
-function randomAjective() {
-  var adjectives = ['hushed','teeny-tiny','decisive','long-term','alcoholic'];
-  return adjectives[Math.floor(generator.random() * adjectives.length)];
-}
-
-function randomAdverb() {
-  var adverbs = ['dimly','acidly','potentially','judgementally','fervently'];
-  return adverbs[Math.floor(generator.random() * adverbs.length)];
-}
-
-function aToAnIfNeeded(sentence) {
-  if (sentence.match(/^a [aeiou]/) || sentence.match(/^a hour/)) {
-    return sentence.replace(/^a/, 'an');
-  }
-  return sentence
-}
-
-function sentenceA(seed) {
-  return aToAnIfNeeded([randomArticle(seed++),randomNoun(seed++),randomVerb(seed++)].join(' '));
-}
-
-function sentenceB(seed) {
-  return aToAnIfNeeded([randomArticle(seed++),randomAjective(seed++),randomNoun(seed++),randomVerb(seed++)].join(' '));
-}
-
-function sentenceC(seed) {
-  return aToAnIfNeeded([randomArticle(seed++),randomNoun(seed++),randomAdverb(seed++),randomVerb(seed++)].join(' '));
-}
-
-function sentenceD(seed) {
-  return aToAnIfNeeded([randomArticle(seed++),randomAjective(seed++),randomNoun(seed++),randomAdverb(seed++),randomVerb(seed++)].join(' '));
-}
-
-function sentenceE(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ' because ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceF(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ' when ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceG(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ' though ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceH(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ' while ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceI(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ', and ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceJ(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ', but ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceK(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ', so ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceL(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ', after ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function sentenceM(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD];
-  return sentences[Math.floor(generator.random() * sentences.length)](seed++) +
-  ', before ' + sentences[Math.floor(generator.random() * sentences.length)](10 + seed++);
-}
-
-function randomSentence(seed) {
-  sentences = [sentenceA, sentenceB, sentenceC, sentenceD, sentenceE, sentenceF,
-     sentenceG, sentenceH, sentenceI, sentenceJ, sentenceK, sentenceL, sentenceM];
-  sentence = sentences[Math.floor(generator.random() * sentences.length)](seed++);
-  return sentence.charAt(0).toUpperCase() + sentence.slice(1);
-  // return 'seed: ' + seed + '.';
-}
-
-function randomTitle(seed) {
-  return randomSentence(seed);
-}
-
-function randomParagraph(seed) {
-  var sentences = 4 + Math.floor(generator.random() * 5);
-  var paragraph = [];
-  for (var i = 0; i < sentences; i++) {
-    paragraph[i] = randomSentence(seed + i * 100);
-  }
-  return paragraph.join('. ') + '.';
-}
-
-function randomParagraphs(seed) {
-  var paragraphs = 5 + Math.floor(generator.random() * 100);
-  var article = [];
-  for (var i = 0; i < paragraphs; i++) {
-    article[i] = randomParagraph(seed + i * 1000);
-  }
-  return article;
-}
-
-function randomLink(seed, hostname) {
-  var link = [];
-  link['href'] = '/' + randomSentence(seed).replace(/ /g, '/').replace(/,/g, '');
-  var linkSeed = generateSeed(hostname + link['href']);
-  link['title'] = randomTitle(linkSeed);
-  return link;
-}
-
-function randomLinks(seed, hostname) {
-  var generator = new MersenneTwister(seed+10000);
-  var linkCount = 5 + Math.floor(generator.random() * 10);
-  var links = [];
-  for (var i = 0; i < linkCount; i++) {
-    links[i] = randomLink(seed + i * 10000, hostname);
-  }
-  return links;
-}
-
-function generateSeed(path) {
-  var md5 = require('md5');
-  var sum = md5(path);
-  var seed = parseInt(sum.slice(0,7),16) + parseInt(sum.slice(8,15),16) + parseInt(sum.slice(16,23),16) + parseInt(sum.slice(24,31),16);
-  return seed;
-}
-
-function randomPage(req, res) {
-  var seed = generateSeed(req.hostname + req.path);
-
-  var title = randomTitle(seed);
-  var paragraphs = randomParagraphs(seed);
-  var links = randomLinks(seed, req.hostname);
-
-  res.render('random', {title: title, paragraphs: paragraphs, links: links});
-}
-
-router.all('*', randomPage);
 
 module.exports = router;
